337da6a3ca6b639813a266a8799716503cd1d3d9
resinbeard <mrafoster@gmail.com>
2024-11-06 13:00:07 -0800

    adding process management for cyperus-server and baton

--- scripts/cyperus.py
+++ scripts/cyperus.py
@@ -18,11 +18,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Copyright 2024 murray foster '''
 
 import argparse
+import ctypes
 import os
 import signal
 import subprocess
+import threading
+import time
 
 CONFIG_DIRECTORY = "~/.local/share/cyperus"
+
+def _set_pdeathsig(sig = signal.SIGTERM):
+    libc = ctypes.CDLL("libc.so.6")
+    def callable():
+        return libc.prctl(1, sig)
+    return callable
 
 
 class Cyperus:
@@ -30,17 +39,22 @@ class Cyperus:
         self.config_directory = config_directory
         self.config_directory_presets = f"{self.config_directory}/presets"
         self.config_directory_preset_active = f"{self.config_directory}/preset_active"
-        self._setup_directories()
 
     _child_processes = {
         'cyperus-server': {
-            'pid': None
+            'pid': None,
+            'thread': None,
+            'exc': None
         },
         'websocket-relay': {
-            'pid': None
+            'pid': None,
+            'thread': None,
+            'exc': None
         },
         'presets-loader': {
-            'pid': None
+            'pid': None,
+            'thread': None,
+            'exc': None
         }
     }
 
@@ -48,33 +62,96 @@ class Cyperus:
         os.makedirs(os.path.expanduser(self.config_directory), exist_ok=True)
         os.makedirs(os.path.expanduser(self.config_directory_presets), exist_ok=True)
         os.makedirs(os.path.expanduser(self.config_directory_preset_active), exist_ok=True)
-    
-    def _setup_cyperus_server(self,
+
+    def _cyperus_server_thread(self,
+                               server_input_channels,
+                               server_output_channels,
+                               server_bitdepth,
+                               server_recv_port,
+                               server_send_port,
+                               server_period):        
+        try:
+            proc = subprocess.Popen(
+                [
+                    'modules/cyperus-server/build/cyperus-server',
+                    '-i', str(server_input_channels),
+                    '-o', str(server_output_channels),
+                    '-bd', str(server_bitdepth),
+                    '-rp', str(server_recv_port),
+                    '-sp', str(server_send_port),
+                    '-p', str(server_period)
+                ],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                preexec_fn=_set_pdeathsig(signal.SIGTERM)
+            )
+
+            self._child_processes['cyperus-server']['pid'] = proc.pid
+            (output, error) = proc.communicate()
+            if error:
+                print('error:', error)
+                print('output:', output)
+                self._child_processes['cyperus-server']['exc'] = Exception(f"{error}: {output}")
+        except Exception as exc:
+            self._child_processes['cyperus-server']['exc'] = exc
+            print('exc', exc)
+            return
+        
+    def _run_cyperus_server(self,
                              server_input_channels=4,
                              server_output_channels=4,
                              server_bitdepth=24,
                              server_recv_port=7500,
                              server_send_port=7400,
                              server_period=256):
-        
-        proc = subprocess.Popen(
-            [
-                'cyperus-server/build/cyperus-server',
-                '-i', str(server_input_channels),
-                '-o', str(server_output_channels),
-                '-bd', str(server_bitdepth),
-                '-rp', str(server_recv_port),
-                '-sp', str(server_send_port),
-                '-p', str(server_period)
-            ],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        self.child_processes['cyperus-server']['pid'] = proc.pid
-        (output, error) = proc.communicate()
-        if error:
-            print('error:', error)
-            print('output:', output)
-            
+        args = (server_input_channels,
+                server_output_channels,
+                server_bitdepth,
+                server_recv_port,
+                server_send_port,
+                server_period)
+        self._child_processes['cyperus-server']['thread'] = threading.Thread(
+            target=self._cyperus_server_thread,
+            args=args)
+        self._child_processes['cyperus-server']['thread'].start()
+
+    def _websocket_relay_thread(self,
+                                relay_recv_port,
+                                relay_send_port):        
+        try:
+            proc = subprocess.Popen(
+                [
+                    'python3', 'modules/baton/baton.py',
+                    '--recvport', str(relay_recv_port),
+                    '--sendport', str(relay_send_port)
+                ],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                preexec_fn=_set_pdeathsig(signal.SIGTERM)
+            )
+
+            self._child_processes['websocket-relay']['pid'] = proc.pid
+            (output, error) = proc.communicate()
+            if error:
+                print('error:', error)
+                print('output:', output)
+                self._child_processes['websocket-relay']['exc'] = Exception(f"{error}: {output}")
+        except Exception as exc:
+            self._child_processes['websocket-relay']['exc'] = exc
+            print('exc', exc)
+            return
+        
+    def _run_websocket_relay(self,
+                             server_recv_port=7400,
+                             server_send_port=7500):
+
+        args = (server_recv_port,
+                server_send_port)
+        self._child_processes['websocket-relay']['thread'] = threading.Thread(
+            target=self._websocket_relay_thread,
+            args=args)
+        self._child_processes['websocket-relay']['thread'].start()
+
+        
     def _setup_websocket_relay(self):
         pass
 
@@ -93,12 +170,20 @@ class Cyperus:
             presets_recv_port=7600,
             presets_send_port=7500):
         self._setup_directories()
-        self._setup_cyperus_server(server_input_channels,
-                                   server_output_channels,
-                                   server_bitdepth,
-                                   server_recv_port,
-                                   server_send_port,
-                                   server_period)
+        self._run_cyperus_server(server_input_channels,
+                                 server_output_channels,
+                                 server_bitdepth,
+                                 server_recv_port,
+                                 server_send_port,
+                                 server_period)
+        self._run_websocket_relay(relay_recv_port,
+                                  relay_send_port)
+        while(True):
+            if self._child_processes['cyperus-server']['exc']:
+                raise(self._child_processes['cyperus-server']['exc'])
+            if self._child_processes['websocket-relay']['exc']:
+                raise(self._child_processes['websocket-relay']['exc'])            
+            time.sleep(0.01)
 
 
 if __name__ == "__main__":
